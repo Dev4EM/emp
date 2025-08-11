be0f59eea769f13aaa0a2b49a9eaea63123f66f4
om2123 <omvakhare150@gmail.com>
2025-08-11 15:33:37 +0530

    feat: Implement JWT authentication middleware and update user model methods

--- backend/models/User.js
+++ backend/models/User.js
@@ -17,19 +17,10 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-// Hash password before saving
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
 
-// Method to compare passwords
+// Method to compare passwords using simple string comparison
 UserSchema.methods.comparePassword = async function(enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+  // This is a direct, plain-text comparison.
+  return enteredPassword === this.password;
 };
-
 module.exports = mongoose.model('User', UserSchema);
